362ecb399a6056aa140bc76dddd255dd67ccdb52
Taam <Taam@TAAM>
2022-11-03 00:47:09 +0700

    Add project files.

---
+++ Data/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinhTam.Data
+{
+    [Table ("User")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public string Age { get; set; }
+    }
+}
---
+++ Data/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinhTam.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext>opt): base(opt)
+        {
+
+
+        }
+        #region DbSet
+        public DbSet<User> ? Users { get; set; }
+        #endregion
+    }
+}
